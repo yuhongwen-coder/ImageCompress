61dae9861aec8dbad7b42d06c272b1556839f154
yuhongwen <15071469547@163.com>
2020-12-03 19:27:36 +0800

    init WebSocket

---
+++ lib_ui/src/main/java/com/application/image/lib_ui/material_design/recylerview/slide/RecylerViewSlideActivity.java
@@ -0,0 +1,11 @@
+package com.application.image.lib_ui.material_design.recylerview.slide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by yuhongwen
+ * on 2020/12/3
+ *  RecylerView 仿照 QQ 侧滑
+ */
+public class RecylerViewSlideActivity extends AppCompatActivity {
+}
